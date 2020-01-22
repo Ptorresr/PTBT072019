1d01c74f64a59ddb3b1b2f9122b04eb6827ff411
Ricardo Torres <rictor@cuhrt.com>
2020-01-21 19:41:51 -0600

    Agregando script de repaso para Sesion-02

---
+++ Sesion-02/Repaso/tabla-n.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Imprime la tabla de multiplicar del número N, el valor de N se solicita
+al usuario y se tiene que validar que el valor proporcionado por el usuario
+sea un número entero y mayor que cero.
+
+Tiempo estimado: 10 mins
+"""
+
