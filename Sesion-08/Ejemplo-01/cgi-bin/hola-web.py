66025fa0b8a668c6ca53c6958c31598633692869
Ricardo Torres <rictor@cuhrt.com>
2020-02-13 19:23:52 -0600

    Agregando archivos y reto para Sesion-08

---
+++ Sesion-08/Ejemplo-01/cgi-bin/hola-web.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print("Content-type: image/jpeg")
+print()
+print("<h1>Hola mundo web en español!</h1>")
+
---
+++ Sesion-08/Reto/cgi-bin/hola-web.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+RETO: Modificar el script para mostrar un archivo de imagen vía web
+"""
+
+print("Content-type: image/jpeg")
+print()
+print("<h1>Hola mundo web en español!</h1>")
+
