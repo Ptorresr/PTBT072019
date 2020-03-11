8ff5848d95b43e0a7b8bf732b226aef5b499d349
Ricardo Torres <rictor@cuhrt.com>
2020-03-10 22:09:38 -0600

    Agregando archivos de la sesión en vivo 16

--- Sesion-16/Bedutravels/tours/urls.py
+++ Sesion-16/Bedutravels/tours/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),  # / -> views.index()
     path("perfiles", views.perfiles, name="perfiles"),  # /perfiles -> views.perfiles()
+    path("login", views.login_user, name="login"),  # /login -> views.login
 ]  # Lista de rutas de la app -tours-
--- Sesion-16/Bedutravels/tours/views.py
+++ Sesion-16/Bedutravels/tours/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-from .models import Tour
+from .models import Tour, User
 
 # Create your views here.
+@login_required()
 def index(request):
     """ Atiende la petición GET / """
 
@@ -16,3 +19,26 @@ def index(request):
 def perfiles(request):
     """ Atiende la petición GET /perfiles """
     return render(request, "tours/perfil.html")  # templates/tours/perfil.html
+
+def login_user(request):
+    """ Atiende la petición GET /login """
+    # Inicializando variable msg a vacio
+    msg = ""
+
+    # Obtener los datos del formulario
+    if request.method == "POST":
+        user_form = (request.POST["username"], request.POST["password"])
+        next_url = request.GET.get("next", "/")
+        user = authenticate(username=user_form[0], password=user_form[1])
+        if user is not None:
+            # Se inicializan las variables de sesión
+            login(request, user)
+            return redirect(next_url)
+        else:
+            msg = "Error datos de acceso inválidos!"
+
+    return render(request, "registration/login.html",
+        {
+            "msg":msg
+        }
+    )
